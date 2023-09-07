43a1955e135f7cbd5de31cd6be4667c5bddff639
WSL2 PC PL <plrichard@me.com>
2023-09-07 15:46:42 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("The script is working");
+const button = document.querySelector('button');
+
+// button.addEventListener('click',() => button.classList.toggle('altcolor'));
+// button.addEventListener('click',console.log('clicked on the button'));
+
+button.addEventListener('click', () => button.classList.toggle('altColor'));
