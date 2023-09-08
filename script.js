43dc950b7907cec62d1735adda8ea28a6e62e9be
WSL2 PC PL <plrichard@me.com>
2023-09-08 10:59:35 -0400

    sticky menu

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
 console.log("The script is working");
-const button = document.querySelector('button');
 
 // button.addEventListener('click',() => button.classList.toggle('altcolor'));
 // button.addEventListener('click',console.log('clicked on the button'));
+// button.addEventListener('click', () => button.classList.toggle('altColor'));
 
-button.addEventListener('click', () => button.classList.toggle('altColor'));
+const menuToggle = document.getElementById('menu-toggle');
+const sideMenu = document.getElementById('side-menu');
+
+menuToggle.addEventListener('click', () => {
+    if (sideMenu.style.left === '-250px' || !sideMenu.style.left) {
+        sideMenu.style.left = '0';
+    } else {
+        sideMenu.style.left = '-250px';
+    }
+});
+
+
+
+// // JavaScript to change the background color of the sticky header and side menu when scrolling
+// const stickyHeader = document.querySelector('.sticky-header');
+// const sideMenu = document.querySelector('.side-menu');
+// const hero = document.querySelector('.hero');
+
+// window.addEventListener('scroll', () => {
+//     const scrollPosition = window.scrollY;
+//     const heroHeight = hero.clientHeight;
+
+//     if (scrollPosition > heroHeight) {
+//         stickyHeader.style.backgroundColor = '#8B4513'; // Brown color
+//         sideMenu.style.backgroundColor = '#8B4513'; // Brown color
+//     } else {
+//         stickyHeader.style.backgroundColor = 'transparent';
+//         sideMenu.style.backgroundColor = 'transparent';
+//     }
+// });
