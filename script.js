19906324d6588bcc3d64bc05253ad1c3b454c24a
Pier-Luc Richard <pier-lucrichard@90-richard-pierluc.local>
2023-09-08 14:07:58 -0400

    the website is now billangual

--- script.js
+++ script.js
@@ -14,22 +14,3 @@ menuToggle.addEventListener('click', () => {
         sideMenu.style.right = '-250px';
     }
 });
-
-
-// // JavaScript to change the background color of the sticky header and side menu when scrolling
-// const stickyHeader = document.querySelector('.sticky-header');
-// const sideMenu = document.querySelector('.side-menu');
-// const hero = document.querySelector('.hero');
-
-// window.addEventListener('scroll', () => {
-//     const scrollPosition = window.scrollY;
-//     const heroHeight = hero.clientHeight;
-
-//     if (scrollPosition > heroHeight) {
-//         stickyHeader.style.backgroundColor = '#8B4513'; // Brown color
-//         sideMenu.style.backgroundColor = '#8B4513'; // Brown color
-//     } else {
-//         stickyHeader.style.backgroundColor = 'transparent';
-//         sideMenu.style.backgroundColor = 'transparent';
-//     }
-// });
